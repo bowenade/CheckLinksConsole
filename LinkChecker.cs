c9ce106a6bd89496a8b1aacf242eb32f7f520595
Bowen <lbwde96@gmail.com>
2022-02-03 11:20:15 +1100

    Link Checker

---
+++ LinkChecker.cs
@@ -0,0 +1,14 @@
+﻿using HtmlAgilityPack;
+public class LinkChecker
+{
+    public static IEnumerable<String> GetLinks(String page)
+    {
+        var htmlDocument = new HtmlDocument();
+        htmlDocument.LoadHtml(page);
+        var links = htmlDocument.DocumentNode.SelectNodes("//a[@href]")
+            .Select(n => n.GetAttributeValue("href", string.Empty))
+            .Where(l => !string.IsNullOrEmpty(l))
+            .Where(l => l.StartsWith("http"));
+        return links;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Http;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var site = "http://www.google.com.au";
+        var client = new HttpClient();
+        var body = client.GetStringAsync(site);
+        Console.WriteLine(body.Result);
+        Console.WriteLine();
+        Console.WriteLine("Links");
+        var links = LinkChecker.GetLinks(body.Result);
+        links.ToList().ForEach(link => Console.WriteLine(link));
+    }
+}
