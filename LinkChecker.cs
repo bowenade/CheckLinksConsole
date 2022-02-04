616c932d1f1ec664f662eb44c35a1ed6083879a1
Bowen <lbwde96@gmail.com>
2022-02-04 13:55:21 +1100

    Checker

--- LinkChecker.cs
+++ LinkChecker.cs
@@ -11,4 +11,44 @@ public class LinkChecker
             .Where(l => l.StartsWith("http"));
         return links;
     }
+
+    public static IEnumerable<LinkCheckResult> CheckLinks(IEnumerable<string> links)
+    {
+        var all = Task.WhenAll(links.Select(CheckLinks));
+        return all.Result;
+    }
+
+    public static async Task<LinkCheckResult> CheckLinks(string link)
+    {
+        var result = new LinkCheckResult();
+        result.Link = link;
+        using (var client = new HttpClient())
+        {
+            var reqeust = new HttpRequestMessage(HttpMethod.Head, link);
+            try
+            {
+                var response = await client.SendAsync(reqeust); 
+                result.Problem = response.IsSuccessStatusCode
+                    ? null
+                    : response.StatusCode.ToString();
+                return result;
+            }   
+            catch (HttpRequestException exception)
+            {
+                result.Problem = exception.Message;
+                return result;
+            }
+        }
+            return result;
+
+    }
+
+}
+
+public class LinkCheckResult
+{
+    public bool Exists => String.IsNullOrWhiteSpace(Problem);
+    public string Problem { get; set; }
+    public string Link { get; set; }
+    public bool IsMissing => !Exists;
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Net.Http;
-
+using System.IO;
 class Program
 {
     static void Main(string[] args)
     {
+        var currentDirectory = Directory.GetCurrentDirectory();
+        var outputFolder = "resports";
+        var outputFile = "report.txt";
+        var outputPath = Path.Combine(currentDirectory, outputFolder, outputFile);
+        var directory = Path.GetDirectoryName(outputPath);
+        Directory.CreateDirectory(directory);
+        Console.WriteLine($"Saving report to {outputPath}");
+
         var site = "http://www.google.com.au";
         var client = new HttpClient();
         var body = client.GetStringAsync(site);
@@ -13,5 +21,16 @@ class Program
         Console.WriteLine("Links");
         var links = LinkChecker.GetLinks(body.Result);
         links.ToList().ForEach(link => Console.WriteLine(link));
+        // write out links
+        // File.WriteAllLines(outputPath, links);
+        var checkedLinks = LinkChecker.CheckLinks(links);
+        using (var file = File.CreateText(outputPath))
+        {
+            foreach (var link in checkedLinks)
+            {
+                var status = link.IsMissing ? "missing" : "OK";
+                file.WriteLine($"{status} - {link.Link}");
+            }
+        }
     }
 }
